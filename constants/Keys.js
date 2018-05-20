b8056dc07ba7f8f06b2d8a5127ddf8dbad85ba91
CarlosWirama <vincentius_carlos@yahoo.co.id>
2018-05-20 15:30:26 +0700

    implement search functionality

---
+++ constants/Keys.js
@@ -0,0 +1,3 @@
+export default {
+  NYTimes: '7bb6006970f94dc79a5f04ba7b16e2f2',
+};
--- screens/ArticlesScreen.js
+++ screens/ArticlesScreen.js
@@ -1,14 +1,71 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity,
-  View } from 'react-native';
+  View, ActivityIndicator, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import Keys from '../constants/Keys';
 
-export default class HomeScreen extends React.Component {
+export default class ArticlesScreen extends React.Component {
+
   static navigationOptions = {
     title: 'Articles',
-  };
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      searchString: '',
+      articles: [],
+      sort: 'newest',
+      isLoading: false,
+    };
+    this.onChangeSearch.bind(this);
+  }
+
+  componentDidMount() {
+    // fetch articles
+    this.search();
+    //const articles = [{key: 'a'}, {key: 'b'}];
+  }
+
+  onChangeSearch = txt => this.setState({searchString:txt})
+
+  toggleSort = () => {
+    const { sort, searchString } = this.state;
+    const newSortRule = (sort === 'newest') ? 'oldest' : 'newest';
+    this.setState({sort: newSortRule});
+    this.search(searchString, newSortRule);
+  }
+  
+  onSearch = () => this.search(this.state.searchString)
+
+  search = (q = '', sort = 'newest') => {
+    let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    const params = {
+      q, sort,
+      'api-key': Keys.NYTimes,
+    };
+
+    const querystring = Object.keys(params)
+      .map(key => key + '=' + encodeURIComponent(params[key]))
+      .join('&');
+
+    console.log(`${url}?${querystring}`);
+    this.setState({isLoading: true});
+    fetch(`${url}?${querystring}`)
+      .then(response => response.json())
+      .then(json => this.updateList(json.response.docs))
+      .finally( () => this.setState({isLoading: false}) );
+  }
+
+  updateList = articles => this.setState({articles})
+
+  _keyExtractor = (item, index) => index
+
+  _renderItem = ({ item, index }) =>
+    <ListItem index={index} item={item} />
 
   render() {
+    console.log(this.state.articles)
     return (
       <View style={styles.container}>
 
@@ -16,25 +73,56 @@ export default class HomeScreen extends React.Component {
         <View style={styles.searchInputContainer}>
           <TextInput
             style={styles.searchInput}
-            onChangeText={(text) => this.setState({text})}
-            // value={this.state.text}
+            onChangeText={this.onChangeSearch}
+            value={this.state.searchString}
+            onSubmitEditing={this.onSearch}
           />
-          <TouchableOpacity style={styles.searchIcon}>
+          <TouchableOpacity
+            style={styles.searchIcon}
+            onPress={this.onSearch}
+          >
             <Ionicons name="ios-search" size={32} color="gray" />
           </TouchableOpacity>
         </View>
 
         {/* Sort / Filter Options */}
-        <TouchableOpacity style={styles.largeButton}>
-          <Text style={styles.largeButtonText}>Sort by: Newest</Text>
+        <TouchableOpacity
+          style={styles.largeButton}
+          onPress={this.toggleSort}
+        >
+          <Text style={styles.largeButtonText}>
+            Sort by: {this.state.sort}
+          </Text>
         </TouchableOpacity>
-        <FlatList
-          data={[{key: 'a'}, {key: 'b'}]}
-          renderItem={({item}) => <Text>{item.key}</Text>}
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
-        />
+        { this.state.isLoading ?
+          <ActivityIndicator size='large'/>
+          :
+          <FlatList
+            data={this.state.articles}
+            renderItem={this._renderItem}
+            style={styles.container}
+            contentContainerStyle={styles.listContainer}
+            keyExtractor={this._keyExtractor}
+            extraData={this.state}
+          />
+        }
       </View>
+    );
+  }
+}
+
+class ListItem extends React.PureComponent {
+  // <Text>{props.snippet}</Text>
+  render() {
+    const { item } = this.props;
+    return (
+      <TouchableOpacity
+        onPress={() => Linking.openURL(item.web_url)}
+        style={styles.listItemContainer}
+      >
+        <Text>{item.headline.print_headline}</Text>
+        <Text>{item.snippet}</Text>
+      </TouchableOpacity>
     );
   }
 }
@@ -44,9 +132,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  contentContainer: {
-    padding: 20,
-    backgroundColor:'gray'
+  listContainer: {
+    // backgroundColor:'lightgray'
+  },
+  listItemContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray'
   },
   // h2: {
   //   fontSize: 17,
@@ -57,6 +150,7 @@ const styles = StyleSheet.create({
   searchInput: {
     width: '100%',
     height: 40,
+    padding: 5,
     borderColor: 'gray',
     borderWidth: 1,
   },
@@ -73,11 +167,12 @@ const styles = StyleSheet.create({
   },
   largeButton: {
     padding: 5,
-    backgroundColor: 'red',
+    backgroundColor: 'lightgreen',
   },
   largeButtonText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: 'black',
+    // color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     textAlign: 'center',
   }
